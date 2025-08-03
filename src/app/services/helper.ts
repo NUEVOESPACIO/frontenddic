c52d530b9cddcaff4672cf434957e4c261399ca0
NUEVOESPACIO <sebacap3@hotmail.com>
2025-08-03 16:46:49 -0300

    Porfolio FrontEnd 03.08.2025 - Regular Update

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,4 +1,4 @@
 // aca hay qu señalar donde esta alojado el BackEnd
 //let baserUrl='http://localhost:8080';
-let baserUrl='https://emotional-johanna-nuevoespacio.koyeb.app';
+let baserUrl='https://emotional-johanna-nuevoespacio.koyeb.app'
 export default baserUrl;
